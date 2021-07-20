6c1d80d3f9f12e58ce0c7075162e68ba4eb981f0
Xuehong <xuehong.liu.pdx@gmail.com>
2021-07-20 11:29:34 -0700

    add timezone option

--- checker_api.py
+++ checker_api.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 import pandas as pd
 from datetime import datetime
+import pytz
 
 def check_symbol(symbol):
     ticker = yf.Ticker(symbol)
@@ -39,13 +40,19 @@ def get_stocks(symbols):
             return [[ticker, close_price,open_price,high,low, low-close_price]]
 
 if __name__ == '__main__':
-    # start_time = datetime.now()
     stocks = input('Enter your stocks and separate by comma: ')
+    print('Select timezone here: https://gist.github.com/heyalexej/8bf688fd67d7199be4a1682b3eec7568')
+    time = input('Enter timezone (Leave empty to use default "America/Los_Angeles"): ')
     print()
     output = get_stocks(stocks)
     if output is None:
         pass
     else:
         df = pd.DataFrame(output, columns = ['symbol', 'Previous close', 'Current open', 'high', 'low', 'change'])
+        if time == '':
+            tz = pytz.timezone('America/Los_Angeles')
+        else:
+            tz = pytz.timezone(time)
+        pst_now = datetime.now(tz)
         print(f'Current time is {datetime.now()}')
         print(df)
